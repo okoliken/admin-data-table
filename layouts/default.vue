59dbf0fcb6bc3b6ca086fe359a7223758535f3f1
Okoli Jahbuchim <jahbuchimokoli3@gmail.com>
2022-09-21 08:07:35 +0100

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app dark>
+    <template>
+      <v-tabs>
+        <v-tab>Item One</v-tab>
+        <v-tab>Item Two</v-tab>
+        <v-tab>Item Three</v-tab>
+      </v-tabs>
+    </template>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
